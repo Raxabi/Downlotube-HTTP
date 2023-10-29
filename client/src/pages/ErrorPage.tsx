f29d6bcab918c20c857bbd53692ac4a8b6114c92
Raxabi <beltrandiaztorres@gmail.com>
2023-10-30 00:44:36 +0100

    [ADD] default errro page created

---
+++ client/src/pages/ErrorPage.tsx
@@ -0,0 +1,16 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom"
+import "../public/css/ErrorPage.css"
+
+function ErrorPage() {
+  const error = useRouteError()
+
+  return (
+    <div id="error-page">
+      <h1 style={{width: "max-content", margin: "auto"}}>Vaya...</h1>
+      <h1>Parece que esta ruta no existe ✋🤑</h1>
+      <h2>{isRouteErrorResponse(error) ? `${error.status} | ${error.statusText}` : "Intenta volver al inicio"}</h2>
+    </div>
+  )
+}
+
+export default ErrorPage
